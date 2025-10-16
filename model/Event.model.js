a7043abb4d8a046ff72db8ba1391125826418430
webwizzz <jatin.tomar@somaiya.edu>
2025-10-17 00:57:48 +0530

    fix: change date field type from Date to String in event schema

--- model/Event.model.js
+++ model/Event.model.js
@@ -59,7 +59,7 @@ const eventSchema = new mongoose.Schema(
     },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    date: { type: Date, required: true },
+    date: { type: String, required: true },
     location: { type: String, required: true },
     heroImage: { type: ImageSchema, default: null },
     heroMobileImage: { type: ImageSchema, default: null },
